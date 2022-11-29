e6959753cd4feb31d5fdf94ffee29ce054d651b9
GonzaloPosse <119451474+GonzaloPosse@users.noreply.github.com>
2022-11-29 17:37:10 -0300

    prueba

--- app/main.py
+++ app/main.py
@@ -13,3 +13,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get('/validar/{numero}')
+def validar_capicua(numero:str):
+    respuesta = 'No es capicua'
+
+    if numero == numero[::-1]:
+        respuesta = 'Si es capicua'
+
+    return {
+        'numero':numero,
+        'validacion':respuesta
+    }
